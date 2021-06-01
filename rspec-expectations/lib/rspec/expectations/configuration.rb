85905f9902d48b66a8e50c27ac7424ffe64f3c71
Benoit Tigeot <benoit.tigeot@lifen.fr>
2021-06-01 08:06:02 +0200

    Add deprecation warnings for the removal of should syntax

--- rspec-expectations/lib/rspec/expectations/configuration.rb
+++ rspec-expectations/lib/rspec/expectations/configuration.rb
@@ -19,7 +19,7 @@ module RSpec
     #   # or
     #
     #   RSpec::Expectations.configuration
-    class Configuration
+    class Configuration # rubocop:disable Metrics/ClassLength
       # @private
       FALSE_POSITIVE_BEHAVIOURS =
         {
@@ -45,7 +45,12 @@ module RSpec
       #       c.syntax = [:should, :expect]
       #     end
       #   end
-      def syntax=(values)
+      def syntax=(values) # rubocop:disable Metrics/MethodLength
+        if self.class.warn_about_syntax?
+          RSpec.deprecate('Expectations syntax configuration',
+                          :replacement => 'the default `expect` syntax',
+                          :call_site => nil)
+        end
         if Array(values).include?(:expect)
           Expectations::Syntax.enable_expect
         else
@@ -53,11 +58,28 @@ module RSpec
         end
 
         if Array(values).include?(:should)
+          if self.class.warn_about_syntax?
+            RSpec.deprecate('`:should` Expectations syntax',
+                            :replacement => 'the default `expect` syntax',
+                            :call_site => nil)
+          end
           Expectations::Syntax.enable_should
         else
           Expectations::Syntax.disable_should
         end
       end
+
+      # @private
+      def self.warn_about_syntax?
+        @warn_about_syntax
+      end
+
+      # @private
+      def self.warn_about_syntax!
+        @warn_about_syntax = true
+      end
+
+      @warn_about_syntax = false
 
       # Configures the maximum character length that RSpec will print while
       # formatting an object. You can set length to nil to prevent RSpec from
@@ -156,7 +178,6 @@ module RSpec
       # @private
       def reset_syntaxes_to_default
         self.syntax = [:should, :expect]
-        RSpec::Expectations::Syntax.warn_about_should!
       end
 
       # @api private
@@ -233,5 +254,6 @@ module RSpec
 
     # set default syntax
     configuration.reset_syntaxes_to_default
+    Configuration.warn_about_syntax!
   end
 end
